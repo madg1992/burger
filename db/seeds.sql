56b1e94d0c1ef9aadaf3d49b6f0a096271198703
Mark Gallardo <mrk_gllrd@yahoo.com>
2019-12-14 14:32:13 -0600

    added SQL queries to seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The House Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Veggie Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Quarter Pounder', TRUE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Cheeseburger', TRUE);
+
